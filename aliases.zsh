9e1ff43ef2814b88f74af90ef866561b303ebb9c
Tobias Weiss <tobias@tobias-weiss.org>
2017-09-20 21:36:06 +0200

    added temp aliases and replaced tmux super key

---
+++ aliases.zsh
@@ -0,0 +1,17 @@
+# aliases for Tmux
+alias tmux='tmux -2'
+alias tmuxi='tmuxinator'
+alias ta='tmux attach -t'
+alias tnew='tmux new -s'
+alias tls='tmux ls'
+alias tkill='tmux kill-session -t'
+
+# convenience aliases for editing configs
+alias ev='vim ~/.vimrc'
+alias evp='vim ~/.vimrc.python'
+alias et='vim ~/.tmux.conf'
+alias ez='vim ~/.zshrc'
+alias aliases='vim ~/.oh-my-zsh/lib/aliases.zsh && . ~/.oh-my-zsh/lib/aliases.zsh'
+
+# jump to actual working dir
+alias act='cd ~/repo/01_coden/python'
