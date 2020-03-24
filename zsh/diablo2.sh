b879eb409265821980e5ae6abb9f2142568efe57
Tobias Weiss <tobias@tobias-weiss.org>
2020-03-24 17:58:43 +0100

    autocommit

--- zsh/diablo2.sh
+++ zsh/diablo2.sh
@@ -1,6 +1,7 @@
 diablo2()
 {
-~/gitfull.sh
+cd ~/dotfiles/d2_save
+git pull
 if [ $(hostname) = "tobi-yoga" ]; then
     sudo mount /opt/Diablo2/LOD-CD/diablo2-LoD.iso /mnt
     wine '/opt/Diablo2/Diablo II.exe' > /dev/null 2>&1
@@ -9,6 +10,11 @@ if [ $(hostname) = "ThinkPad.local.tobias-weiss.org" ]; then
     sudo mount /media/weiss/Windows8_OS/Diablo2_CDs/LOD/diablo2-LoD.iso /mnt
     wine '/media/weiss/Windows8_OS/Diablo2/Diablo II.exe' > /dev/null 2>&1
 fi
-cp /media/weiss/Windows8_OS/Diablo2/ ~/dotfiles/d2_save
+}
+
+save_d2()
+{
+cp /media/weiss/Windows8_OS/Diablo2/Save/* ~/dotfiles/d2_save
+git commit -m "d2 save" && git push
 ~/gitfull.sh
 }
