defc2bf416819707177bac3058151ff088c1a500
Tobias Weiss <tobias@tobias-weiss.org>
2020-11-12 18:09:07 +0100

    add legion zsh config

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -4,7 +4,33 @@ SAVEHIST=1000
 setopt inc_append_history # To save every command before it is executed 
 setopt share_history # setopt inc_append_history
 export KEYTIMEOUT=1 # set timeout for esc key to 0.1
-export PATH=$PATH:$HOME/dotfiles/utils:/home/weiss/.gem/ruby/2.7.0/bin:/home/weiss/.local/bin
+
+# PATH Settings
+if [[ $HOST = "tobi-legion" ]]; then
+	export PATH=$PATH:$HOME/dotfiles/utils:/home/weiss/bin:/home/weiss/.local/bin
+	export VISUAL=/usr/bin/nvim
+	export EDITOR=/usr/bin/nvim
+fi
+if [[ $HOST = "ewf-psl3" || $HOST = "lab" ]]; then
+	alias vi="nvim"
+	alias vim="nvim"
+fi
+# Settings
+if [[ $HOST = "ThinkPad.local.tobias-weiss.org" || $HOST = "tobi-yoga" ]]; then
+	export PATH=$PATH:$HOME/dotfiles/utils:/home/weiss/.gem/ruby/2.7.0/bin:/home/weiss/.local/bin
+	export VISUAL=/usr/bin/nvim
+	export EDITOR=/usr/bin/nvim
+fi
+
+# Aliases
+if [[ $HOST = "ThinkPad.local.tobias-weiss.org" || $HOST = "tobi-yoga"  || $HOST = "tobi-legion" ]]; then
+	alias vi="nvim"
+	alias vim="nvim"
+	alias lab="tmuxinator lab"
+	alias ba="tmuxinator ba"
+	#alias labor="ssh-add ~/.ssh/id_uni && ssh labor"
+	alias speakers="rfkill unblock bluetooth && bluetoothctl power on && a2dp.py CC:98:8B:D1:BD:D2 -t 4 -w 1 -p hsp"
+fi
 
 # git
 git config --global push.default current
@@ -23,31 +49,11 @@ if [[ "${terminfo[kcud1]}" != "" ]]; then
 	bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
 fi
 
-# Aliases
-if [[ $HOST = "ThinkPad.local.tobias-weiss.org" || $HOST = "tobi-yoga" ]]; then
-	alias vi="nvim"
-	alias vim="nvim"
-	alias lab="tmuxinator lab"
-	alias ba="tmuxinator ba"
-	#alias labor="ssh-add ~/.ssh/id_uni && ssh labor"
-	alias speakers="rfkill unblock bluetooth && bluetoothctl power on && a2dp.py CC:98:8B:D1:BD:D2 -t 4 -w 1 -p hsp"
-fi
-
-if [[ $HOST = "ewf-psl3" || $HOST = "lab" ]]; then
-	alias vi="nvim"
-	alias vim="nvim"
-fi
-
 mkdir -p /tmp/log
 
 # This is currently causing problems (fails when you run it anywhere that isn't a git project's root directory)
 # alias vs="v `git status --porcelain | sed -ne 's/^ M //p'`"
 
-# Settings
-if [[ $HOST = "ThinkPad.local.tobias-weiss.org" || $HOST = "tobi-yoga" ]]; then
-	export VISUAL=/usr/bin/nvim
-	export EDITOR=/usr/bin/nvim
-fi
 
 #Functions
 # Loop a command and show the output in vim
@@ -118,6 +124,12 @@ if [[ $HOST = "ThinkPad.local.tobias-weiss.org" || $HOST = "tobi-yoga" ]]; then
   export PYTHONPATH="${SPARK_HOME}/python:${SPARK_HOME}/python/lib/py4j-0.10.4-src.zip:${PYTHONPATH}"
   export PATH="${SPARK_HOME}/python:${PATH}"
 fi
+if [[ $HOST = "tobi-legion" ]]; then
+  export SPARK_HOME="/usr/local/src/spark-3.0.1-bin-hadoop2.7"
+  export PYTHONPATH="${SPARK_HOME}/python:${SPARK_HOME}/python/lib/py4j-0.10.4-src.zip:${PYTHONPATH}"
+  export PATH="${SPARK_HOME}/python:${PATH}"
+fi
+
 
 #gurobi optimizer
 if [[ $HOST = "ThinkPad.local.tobias-weiss.org" ]]; then
