77593a58b19dd735aa72ba56f66d36f1cb162230
Tobias Weiss <tobias@tobias-weiss.org>
2021-01-31 00:02:11 +0100

    update for diablo on legion

--- zsh/diablo2.sh
+++ zsh/diablo2.sh
@@ -1,11 +1,11 @@
 diablo2()
 {
-if [ $(hostname) = "tobi-yoga" || $(hostname) = "tobi-legion" ]; then
+if [[ $HOST = "tobi-yoga" || $HOST = "tobi-legion" ]]; then
     sudo mount /opt/Diablo2/LOD-CD/diablo2-LoD.iso /mnt
     cd '/opt/Diablo2/Mod PlugY/'
     wine '/opt/Diablo2/Mod PlugY/PlugY.exe' > /dev/null 2>&1
 fi
-if [ $(hostname) = "ThinkPad.local.tobias-weiss.org" ]; then
+if [[ $HOST = "ThinkPad.local.tobias-weiss.org" ]]; then
     cd '/media/weiss/Windows8_OS/Diablo2/Mod PlugY'
     sudo mount /media/weiss/Windows8_OS/Diablo2_CDs/LOD/diablo2-LoD.iso /mnt
     wine '/media/weiss/Windows8_OS/Diablo2/Mod PlugY/PlugY.exe' > /dev/null 2>&1
@@ -14,10 +14,10 @@ fi
 
 save_d2()
 {
-if [ $(hostname) = "ThinkPad.local.tobias-weiss.org" ]; then
+if [[ $HOST = "ThinkPad.local.tobias-weiss.org" ]]; then
     cp /media/weiss/Windows8_OS/Diablo2/Save/* ~/dotfiles/d2_save/
 fi
-if [ $(hostname) = "tobi-yoga" ]; then
+if [[ $HOST = "tobi-yoga" ]]; then
     cp  /opt/Diablo2/Save/* ~/dotfiles/d2_save/ 
 fi
     cd ~/dotfiles
@@ -30,10 +30,10 @@ restore_d2()
 {
 cd ~/dotfiles
 git pull
-if [ $(hostname) = "tobi-yoga" || $(hostname) = "tobi-legion" ]; then
+if [[ $HOST = "tobi-yoga" || $HOST = "tobi-legion" ]]; then
     cp ~/dotfiles/d2_save/* '/opt/Diablo2/Save/' 
 fi
-if [ $(hostname) = "ThinkPad.local.tobias-weiss.org" ]; then
+if [[ $HOST = "ThinkPad.local.tobias-weiss.org" ]]; then
     cp ~/dotfiles/d2_save/* '/media/weiss/Windows8_OS/Diablo2/Save/' 
 
 fi
