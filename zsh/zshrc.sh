bef7d44e244a80667e9747d0c937af1f703e71e5
Tobias Weiss <tobias@tobias-weiss.org>
2023-01-15 20:49:01 +0100

    autocommit

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -52,6 +52,24 @@ if [[ $HOST = "tobi-legion" ]]; then
     export PYENV_ROOT="$HOME/.pyenv"
     command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init -)"
+
+    # >>> mamba initialize >>>                                     
+    # !! Contents within this block are managed by 'mamba init' !!                                                                 
+    export MAMBA_EXE="/home/weiss/.local/bin/micromamba";                                                                          
+    export MAMBA_ROOT_PREFIX="/home/weiss/micromamba";                                                                             
+    __mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"                             
+    if [ $? -eq 0 ]; then                                                                                                          
+        eval "$__mamba_setup"                                                                                                      
+    else                                                                                                                           
+        if [ -f "/home/weiss/micromamba/etc/profile.d/micromamba.sh" ]; then                                                       
+            . "/home/weiss/micromamba/etc/profile.d/micromamba.sh"                                                                 
+        else                                                                                                                       
+            export  PATH="/home/weiss/micromamba/bin:$PATH"  # extra space after export prevents interference from conda init      
+        fi                                                                                                                         
+    fi                                                                                                                             
+    unset __mamba_setup                                                                                                            
+    # <<< mamba initialize <<< 
+
 fi
 
 if [[ $HOST = "tobi-yoga" ]]; then
