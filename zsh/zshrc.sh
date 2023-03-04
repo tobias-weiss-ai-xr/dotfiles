6dc11716eb07b63a3e1f2bb3470a351d5b459e20
Tobias Weiss <tobias@tobias-weiss.org>
2023-03-04 22:39:05 +0100

    autocommit

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -24,19 +24,6 @@ if [[ $HOST = "tobi-yoga"  || $HOST = "tobi-legion" ]]; then
 
     #wacom
     alias wacom="mapwacom -d 'Wacom Intuos PT M Pen stylus' -d 'Wacom Intuos PT M Pen eraser'  -s 'HDMI-0'"
-
-    # >>> conda initialize >>>
-    __conda_setup="${/usr/bin/conda shell.zsh hook 2> /dev/null)}"
-    if [ $? -eq 0 ]; then
-        eval "$__conda_setup"
-    else
-        if [ -f "/usr/etc/profile.d/conda.sh" ]; then
-            . "/usr/etc/profile.d/conda.sh"
-        else
-            export PATH="/usr/bin:$PATH"
-        fi
-    fi
-    unset __conda_setup
 fi
 
 if [[ $HOST = "tobi-legion" ]]; then
